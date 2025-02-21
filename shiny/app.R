84bdf85d94bcf8db4b342f6c8e3d1670565303b3
Darian Gill <dgill@nceas.ucsb.edu>
2025-02-21 13:56:09 -1000

    Add datatable and map to make multi-page app

--- shiny/app.R
+++ shiny/app.R
@@ -1,23 +1,44 @@
 library(shiny)
 library(bslib)
+library(magrittr)  # Add this line to load the magrittr package
 
 # Define UI for app that draws a histogram ----
-ui <- page_sidebar(
+ui <- page_navbar(
   # App title ----
-  title = "Hello world!",
-  # Sidebar panel for inputs ----
-  sidebar = sidebar(
-    # Input: Slider for the number of bins ----
-    sliderInput(
-      inputId = "bins",
-      label = "Number of bins:",
-      min = 1,
-      max = 50,
-      value = 30
+  title = "Ole Faithful!",
+  
+  # First page: Histogram ----
+  nav_panel(
+    title = "Histogram",
+    sidebarLayout(
+      sidebarPanel(
+        # Input: Slider for the number of bins ----
+        sliderInput(
+          inputId = "bins",
+          label = "Number of bins:",
+          min = 1,
+          max = 50,
+          value = 30
+        )
+      ),
+      mainPanel(
+        # Output: Histogram ----
+        plotOutput(outputId = "distPlot")
+      )
     )
   ),
-  # Output: Histogram ----
-  plotOutput(outputId = "distPlot")
+  
+  # Second page: Searchable Table ----
+  nav_panel(
+    title = "Table",
+    DT::dataTableOutput(outputId = "dataTable")
+  ),
+  
+  # Third page: Map ----
+  nav_panel(
+    title = "Map",
+    leaflet::leafletOutput(outputId = "map")
+  )
 )
 
 # Define server logic required to draw a histogram ----
@@ -30,9 +51,9 @@ server <- function(input, output) {
   #
   # 1. It is "reactive" and therefore should be automatically
   #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
+  # 2. Its output type is a plot  
   output$distPlot <- renderPlot({
-
+        
     x    <- faithful$waiting
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
@@ -42,6 +63,17 @@ server <- function(input, output) {
 
     })
 
+  output$dataTable <- DT::renderDataTable({
+    DT::datatable(faithful)
+  })
+
+  output$map <- leaflet::renderLeaflet({
+    leaflet::leaflet() %>%
+      leaflet::addTiles() %>%
+      leaflet::setView(lng = -110.8281, lat = 44.4605, zoom = 10) %>%
+      leaflet::addMarkers(lng = -110.8281, lat = 44.4605,
+                          popup = "Old Faithful!")
+  })
 }
 
 shinyApp(ui = ui, server = server)
