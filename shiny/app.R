8a14d65f1a997eed1f8a5497bfabc97cd1080dd4
Darian Gill <dgill@nceas.ucsb.edu>
2025-02-26 15:52:54 -1000

    Restore state on refresh

--- shiny/app.R
+++ shiny/app.R
@@ -15,128 +15,137 @@ custom_theme <- bs_theme(
   heading_font = font_google("Inter")
 )
 
-# Define UI for app that draws a histogram ----
-ui <- page_navbar(
-  id = "page",
-  theme = custom_theme,  # Apply the custom theme
+# Wrap UI in a function(req) for bookmarking support
+ui <- function(req) {
+  page_navbar(
+    id = "page",
+    theme = custom_theme,  # Apply the custom theme
+    title = "Ole Faithful!",
 
-  # App title ----
-  title = "Ole Faithful!",
-
-  # First page: Histogram ----
-  nav_panel(
-    title = "Histogram",
-    sidebarLayout(
-      sidebarPanel(
-        # Input: Slider for the number of bins ----
-        sliderInput(
-          inputId = "bins",
-          label = "Number of bins:",
-          min = 1,
-          max = 50,
-          value = 30
-        )
-      ),
-      mainPanel(
-        # Output: Histogram ----
-        plotOutput(outputId = "distPlot")
-      )
-    )
-  ),
-
-  # Second page: Searchable Table ----
-  nav_panel(
-    title = "Table",
-    fluidRow(
-      column(6, DT::dataTableOutput(outputId = "dataTable")),  # Table on left
-      column(6, uiOutput(outputId = "cardView"))  # Card view on right
-    )
-  ),
-
-  # Third page: Map ----
-  nav_panel(
-    title = "Map",
-    leaflet::leafletOutput(outputId = "map")
-  ),
-
-  nav_spacer(),
-  nav_menu(
-    title = "About",
-    align = "right",
+    # First page: Histogram
     nav_panel(
-      title = "Old Faithful",
-      includeMarkdown(
-        "/Users/dariangill/git/vegbank-web/shiny/static_content/old_faithful.md"
+      title = "Histogram",
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput(
+            inputId = "bins",
+            label = "Number of bins:",
+            min = 1,
+            max = 50,
+            value = 30
+          )
+        ),
+        mainPanel(
+          plotOutput("distPlot")
+        )
       )
     ),
+
+    # Second page: Searchable Table
     nav_panel(
-      title = "Geysers",
-      includeMarkdown(
-        "/Users/dariangill/git/vegbank-web/shiny/static_content/geysers.md"
+      title = "Table",
+      fluidRow(
+        column(6, DT::dataTableOutput("dataTable")),
+        column(6, uiOutput("cardView"))
+      )
+    ),
+
+    # Third page: Map
+    nav_panel(
+      title = "Map",
+      leafletOutput("map")
+    ),
+
+    nav_spacer(),
+    nav_menu(
+      title = "About",
+      align = "right",
+      nav_panel(
+        title = "Old Faithful",
+        includeMarkdown("/Users/dariangill/git/vegbank-web/shiny/static_content/old_faithful.md")
+      ),
+      nav_panel(
+        title = "Geysers",
+        includeMarkdown("/Users/dariangill/git/vegbank-web/shiny/static_content/geysers.md")
       )
     )
   )
-)
+}
 
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
+server <- function(input, output, session) {
 
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-
+  # Render histogram based on the 'bins' input
   output$distPlot <- renderPlot({
-
-    x    <- faithful$waiting
+    x <- faithful$waiting
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
     hist(x, breaks = bins, col = "#3b8a71", border = "white",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times")
-
   })
 
+  # Restore state for 'bins' and datatable row selection
+  onRestore(function(state) {
+    updateSliderInput(session, "bins", value = state$input$bins)
+    if (!is.null(state$input$dataTable_rows_selected) &&
+        length(state$input$dataTable_rows_selected) > 0) {
+      selected_row <- state$input$dataTable_rows_selected
+      selected_data <- faithful[selected_row, ]
+      output$cardView <- renderUI({
+        tagList(
+          h3(paste("Row", selected_row)),
+          p(paste("Duration of eruption:", selected_data$eruptions, "minutes")),
+          p(paste("Waiting time to next eruption:", selected_data$waiting, "minutes"))
+        )
+      })
+      # Use the DT proxy to update the highlighted row in the datatable
+      dt_proxy <- dataTableProxy("dataTable")
+      selectRows(dt_proxy, selected_row)
+    }
+  })
+
+  # Render the data table
   output$dataTable <- DT::renderDataTable({
-    DT::datatable(faithful, selection = "single")  # Enable single row selection
+    DT::datatable(faithful, selection = "single")
   })
 
+  # Update cardView when a row is selected
   observeEvent(input$dataTable_rows_selected, {
     selected_row <- input$dataTable_rows_selected
     if (length(selected_row) > 0) {
       selected_data <- faithful[selected_row, ]
       output$cardView <- renderUI({
-        card(
-          card_header("Old Faithful Geyser Data"),
-          card_body(
-            h3(paste("Row ", selected_row)),
-            p(paste("Duration of eruption:",
-                    selected_data$eruptions,
-                    "minutes")),
-            p(paste("Waiting time to next eruption:",
-                    selected_data$waiting,
-                    "minutes"))
-          )
+        tagList(
+          h3(paste("Row", selected_row)),
+          p(paste("Duration of eruption:", selected_data$eruptions, "minutes")),
+          p(paste("Waiting time to next eruption:", selected_data$waiting, "minutes"))
         )
       })
     } else {
-      output$cardView <- renderUI({ NULL })  # Hide the card when no selection
+      output$cardView <- renderUI({ NULL })
     }
   })
 
-  output$cardView <- renderUI(({ NULL }))  # Initially hide the card view
+  # Initially hide the card view (in case no row is selected at start)
+  output$cardView <- renderUI({ NULL })
 
+  # Render the map
   output$map <- leaflet::renderLeaflet({
-    leaflet::leaflet() %>%
-      leaflet::addTiles() %>%
-      leaflet::setView(lng = -110.8281, lat = 44.4605, zoom = 10) %>%
-      leaflet::addMarkers(lng = -110.8281, lat = 44.4605,
-                          popup = "Old Faithful!")
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = -110.8281, lat = 44.4605, zoom = 10) %>%
+      addMarkers(lng = -110.8281, lat = 44.4605, popup = "Old Faithful!")
+  })
+
+  # Automatically trigger bookmarking on any input change
+  observe({
+    reactiveValuesToList(input)
+    session$doBookmark()
+  })
+
+  # Update the browser's URL query string when bookmarking completes
+  onBookmarked(function(url) {
+    updateQueryString(url)
   })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server, enableBookmarking = "url")
