4deb91f27ae0923e58987763ff81f27652e6d6e4
Darian Gill <dgill@nceas.ucsb.edu>
2025-04-07 14:53:06 -1000

    Add app.R as in https://mjfrigaard.github.io/shiny-app-pkgs/launch.html to launch via IDE exension

--- R/run_app.R
+++ R/run_app.R
@@ -2,11 +2,18 @@
 #'
 #' Launches the Vegbank Shiny application.
 #'
+#' @family **Standalone Application**
+#' @section Details:
+#' The application uses:
+#' - **UI**: Defined in [`ui.R`].
+#' - **Server Logic**: Defined in [`server.R`].
+#' @return A **Shiny application** object.
 #' @export
-run_app <- function() {
+run_app <- function(options = list()) {
   shiny::shinyApp(
     ui = ui,
     server = server,
-    enableBookmarking = "url"
+    enableBookmarking = "url",
+    options = options
   )
 }
