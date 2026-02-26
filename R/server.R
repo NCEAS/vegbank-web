0720c6abce034bd6714c6f02755604ca4007cd2e
Darian Gill <dgill@nceas.ucsb.edu>
2026-02-25 17:36:30 -1000

    Use R_CONFIG_FILE to specify config

--- R/server.R
+++ R/server.R
@@ -21,7 +21,7 @@
 #' @noRd
 # ================= MAIN SERVER FUNCTION ===========================================================
 server <- function(input, output, session) {
-  cfg <- config::get(file = system.file("config.yml", package = "vegbankweb"))
+  cfg <- config::get()
   if (isTRUE(cfg$vb_debug)) vegbankr::vb_debug()
   vegbankr::vb_set_base_url(cfg$vb_base_url)
 
